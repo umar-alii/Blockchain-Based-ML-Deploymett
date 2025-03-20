a96a5cec6ffbe14c29a02f71e18fdde7a0d8012b
umar-alii <umarrali.pk@gmail.com>
2025-03-20 21:32:21 +0500

    Security

---
+++ ML-Deployment/src/main/java/ML/Deployment/ML/Deployment/security/CustomUserDetailsService.java
@@ -0,0 +1,56 @@
+package ML.Deployment.ML.Deployment.security;
+
+import ML.Deployment.ML.Deployment.exception.ResourceNotFoundException;
+import ML.Deployment.ML.Deployment.model.User;
+import ML.Deployment.ML.Deployment.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User not found with email : " + email)
+                );
+
+        // Convert Role enum to String using getName() or name() method
+        List<GrantedAuthority> authorities = user.getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority(role.name()))
+                .collect(Collectors.toList());
+
+        // Use your UserPrincipal implementation
+        return UserPrincipal.create(user, authorities);
+    }
+
+    @Transactional
+    public UserDetails loadUserById(String id) {
+        User user = userRepository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("User", "id", id)
+        );
+
+        // Convert Role enum to String using getName() or name() method
+        List<GrantedAuthority> authorities = user.getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority(role.name()))
+                .collect(Collectors.toList());
+
+        // Use your UserPrincipal implementation
+        return UserPrincipal.create(user, authorities);
+    }
+
+}
