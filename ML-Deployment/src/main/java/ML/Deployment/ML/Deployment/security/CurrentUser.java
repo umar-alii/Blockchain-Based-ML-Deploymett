11c648a9d32122cf2d80614dae018511724dc0d9
umar-alii <umarrali.pk@gmail.com>
2025-03-24 19:12:13 +0500

    Current User Interface

---
+++ ML-Deployment/src/main/java/ML/Deployment/ML/Deployment/security/CurrentUser.java
@@ -0,0 +1,11 @@
+package ML.Deployment.ML.Deployment.security;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import java.lang.annotation.*;
+
+@Target({ElementType.PARAMETER, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@AuthenticationPrincipal // Meta-annotation
+public @interface CurrentUser {
+}
