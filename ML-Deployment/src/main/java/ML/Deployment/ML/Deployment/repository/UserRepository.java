78df07c37532cfb683c6ad4d80d418076f40e355
umar-4114 <umarr.siddique.pk@gmail.com>
2025-03-27 00:58:31 +0500

    user repo

---
+++ ML-Deployment/src/main/java/ML/Deployment/ML/Deployment/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package ML.Deployment.ML.Deployment.repository;
+
+import ML.Deployment.ML.Deployment.model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+    Optional<User> findByEmail(String email);
+
+    Boolean existsByEmail(String email);
+
+}
